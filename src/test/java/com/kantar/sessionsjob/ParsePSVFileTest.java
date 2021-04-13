c0f525490409ce8f39881a7ee1a577a9c5c4466c
Wojciech Świderski <wojciechswiderski87@gmail.com>
2021-04-13 19:41:21 +0200

    Changed the test on checking the occurence of an exception when input file has no records

--- src/test/java/com/kantar/sessionsjob/ParsePSVFileTest.java
+++ src/test/java/com/kantar/sessionsjob/ParsePSVFileTest.java
@@ -2,32 +2,15 @@ package com.kantar.sessionsjob;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ParsePSVFileTest {
 
     @Test
-    void givenInputFileWhenParseThenProperWrite() throws IOException {
-        //Given
-        String inputFileName = "src/test/resources/input-statements.psv";
-        String outputFileName = "target/actual-sessions.psv";
-        String expectedOutputFileName = "src/test/resources/expected-sessions.psv";
+    void givenEmptyInputFileWhenParseThenThrowException() {
+        String inputFileName = "src/test/resources/input-statements-without-records.psv";
         ParsePSVFile psvFile = new ParsePSVFile();
 
-        //When
-        psvFile.parse(inputFileName);
-        psvFile.writeOutputFile(outputFileName);
-
-        //Then
-        byte[] outputFileBytes = Files.readAllBytes(Paths.get(outputFileName));
-        byte[] expectedOutputFileBytes = Files.readAllBytes(Paths.get(expectedOutputFileName));
-        String output = new String(outputFileBytes, StandardCharsets.UTF_8);
-        String expectedOutput = new String(expectedOutputFileBytes, StandardCharsets.UTF_8);
-        assertEquals(expectedOutput, output);
+        Exception exception = assertThrows(EmptyFileException.class, () -> psvFile.parse(inputFileName));
     }
 }
